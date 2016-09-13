8ca3410a5b5160dcb78fbbf15927da6f7cb99063
Serhii Shemshur <serhiis@softwareplanet.uk.com>
2016-09-13 17:13:24 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var executablePath = "/home/sergiish/Desktop/testdata/StubMicroservice";
+
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = executablePath,
+                    Arguments = "",
+                    UseShellExecute = false,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true,
+                },
+                EnableRaisingEvents = true,
+            };
+            process.Start();
+            process.Kill();
+            process.WaitForExit();
+
+            Console.WriteLine(process.HasExited);
+
+            Console.WriteLine(process.StartTime);
+
+
+        }
+    }
+}
